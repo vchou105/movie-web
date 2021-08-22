1c5c467ba441ba1036a4030fae2b12ea03a16518
Vicky <v2chou@ucsd.edu>
2021-08-23 00:42:41 +0800

    restore previous movie result change

--- src/components/SearchMovies.js
+++ src/components/SearchMovies.js
@@ -6,13 +6,14 @@ function SearchMovies() {
     const [movies, setMovies] = useState([])
     const [query, setQuery] = useState("")
     const [showMovies, setShowMovies] = useState(false)
+    const [page, setPage] = useState(1)
 
     function handleSearch(e) {
         // cancel default submit action of form 
-        e.preventDefault()
+        e && e.preventDefault()
         async function fetchAPI() {
             // url for fetching json data from OMDb api with user input and api key
-            let url = `https://www.omdbapi.com/?s=${query}&apikey=${process.env.REACT_APP_API_KEY}`
+            let url = `https://www.omdbapi.com/?s=${query}&page=${page}&apikey=${process.env.REACT_APP_API_KEY}`
             // make get request (default) to movie api
             await fetch(url)
             .then(response => { // get Promise for json body of http response (not loaded yet)
@@ -23,15 +24,21 @@ function SearchMovies() {
                 // store returned json body of movie search results
                 setMovies(data) 
                 setShowMovies(true)
+                // find number of pages
+                // handlePageOptions() 
                 return data
             })
             .catch(error => { // catch network error or when response is invalid JSON
                 console.error('Error: ', error) 
                 return Promise.reject(error)
             })
-        }
-        
+        }        
         fetchAPI()
+    }
+
+    const handlePageSelected = (p) => {
+        setPage(p) 
+        handleSearch()
     }
 
     return (
@@ -42,7 +49,7 @@ function SearchMovies() {
                 <input type="search" id="movieInput" value={query} className="searchInput" 
                     placeholder="Search movie title...🔍" onChange={e => setQuery(e.target.value)} required/>
             </form>
-            { showMovies ? <MovieResults data={movies}/> : null} 
+            { showMovies && <MovieResults data={movies} search={(p) => handlePageSelected(p)}/> } 
         </div>
     )
 } 
